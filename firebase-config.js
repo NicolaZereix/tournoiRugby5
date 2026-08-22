f83af321ad5ae997c375e14297956180245a3d80
NicolaZereix <fourcade.n@orange.fr>
2026-08-22 11:53:56 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// Configuration Firebase - RefeteAzereixTournoi
 window.TOURNAMENT_FIREBASE_CONFIG = {
   apiKey: "AIzaSyDPNELf99KxzhX0JQt45fBw4rMhOT_CPLE",
   authDomain: "refeteazereixtournoi.firebaseapp.com",
@@ -8,5 +7,4 @@ window.TOURNAMENT_FIREBASE_CONFIG = {
   messagingSenderId: "866069858316",
   appId: "1:866069858316:web:2f210344c40dc5a0adc629"
 };
-
 window.TOURNAMENT_DB_PATH = "tournaments/azereix2026";
